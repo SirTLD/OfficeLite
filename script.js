c758551ca82e0c1cb3c4b9d2140a65eeb8daf6c6
tripol <e.ekanemeno@gmail.com>
2021-03-04 20:06:00 +0100

    add SignUpTitle function To change title of the signup title

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-let countDownDate = new Date('April 5, 2021').getTime();
+document.addEventListener('DOMContentLoaded', () => {
+  let countDownDate = new Date('April 5, 2021').getTime();
 
-// document.getElementById('footer-timer').innerHTML = new Date('April, 5, 2021');
+  // document.getElementById('footer-timer').innerHTML = new Date('April, 5, 2021');
 
-let x = setInterval(function () {
-  let now = new Date().getTime();
+  let x = setInterval(function () {
+    let now = new Date().getTime();
 
-  let distance = countDownDate - now;
+    let distance = countDownDate - now;
 
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById('countdown-days').innerHTML = days;
-  document.getElementById('countdown-hours').innerHTML = hours;
-  document.getElementById('countdown-minutes').innerHTML = minutes;
-  document.getElementById('countdown-seconds').innerHTML = seconds;
+    document.getElementById('countdown-days').innerHTML = days;
+    document.getElementById('countdown-hours').innerHTML = hours;
+    document.getElementById('countdown-minutes').innerHTML = minutes;
+    document.getElementById('countdown-seconds').innerHTML = seconds;
 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById('footer-timer').innerHTML = 'Time is Now';
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById('footer-timer').innerHTML = 'Time is Now';
+    }
+  }, 1000);
+
+  const changeSignUpTitle = () => {
+    let formTitle = document.getElementById('javascript-test');
+
+    if (window.location.href == 'http://127.0.0.1:5500/sign-up.html') {
+      formTitle.innerHTML = 'Greatness shows here';
+    }
   }
-}, 1000);
 
-// let formTitle = document.getElementById('javascript-test');
+  changeSignUpTitle();
 
-// if (window.location.href === 'http://127.0.0.1:5500/sign-up.html') {
-//   formTitle.innerHTML = 'Greatness';
-// }
+  
+  let tablet = matchMedia('(max-width: 700px)');
 
-let tablet = matchMedia('(max-width: 700px)');
-
-tablet.addEventListener('change', (e) => {
-  console.log(e);
+  tablet.addEventListener('change', (e) => {
+    console.log(e);
+  });
 });
+
+
